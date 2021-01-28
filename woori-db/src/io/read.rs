e2b4cf27f669842000747b7f494245e9f7cbc7b1
Julia Naomi <jnboeira@outlook.com>
2021-01-28 11:53:25 -0300

    current file is current date

--- woori-db/src/io/read.rs
+++ woori-db/src/io/read.rs
@@ -3,9 +3,13 @@ use std::{fs::OpenOptions, io::Read};
 
 #[cfg(test)]
 pub fn assert_content(pat: &str) {
+    use chrono::prelude::*;
+    let utc: DateTime<Utc> = Utc::now();
+    let date_log = utc.format("%Y_%m_%d.log").to_string();
+
     let mut file = OpenOptions::new()
             .read(true)
-            .open("current.log").unwrap();
+            .open(date_log).unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
 
--- woori-db/src/io/write.rs
+++ woori-db/src/io/write.rs
@@ -1,11 +1,15 @@
 use std::{fs::OpenOptions, io::Write};
 use std::io::Error;
+use chrono::{DateTime, Utc};
 
 pub async fn write_to_log(log: &str) -> Result<usize, Error> {
+    let utc: DateTime<Utc> = Utc::now();
+    let date_log = utc.format("%Y_%m_%d.log").to_string();
+
     let mut file = OpenOptions::new()
             .append(true)
             .create(true)
-            .open("current.log")?;
+            .open(date_log)?;
     
     let written_bytes = file.write(log.as_bytes())?;
     
