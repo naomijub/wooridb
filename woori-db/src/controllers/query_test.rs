cc486e93b8c7ee9c2f206c390e625af600cbd371
Julia Naomi <jnboeira@outlook.com>
2021-02-05 21:56:28 -0300

    test select ids

--- woori-db/src/controllers/query_test.rs
+++ woori-db/src/controllers/query_test.rs
@@ -234,6 +234,134 @@ async fn test_select_keys_post_ok() {
     assert!(!body.contains("Integer(123)"));
 }
 
+#[ignore]
+#[actix_rt::test]
+async fn test_select_all_ids_post_ok() {
+    let mut app = test::init_service(App::new().configure(routes)).await;
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("CREATE ENTITY test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let _ = test::call_service(&mut app, req).await;
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 123, b: 12.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid1 = &body[(body.len() - 36)..];
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 456, b: 52.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid2 = &body[(body.len() - 36)..];
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 543, b: 32.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid3 = &body[(body.len() - 36)..];
+
+    let payload = format!(
+        "Select * FROM test_select_all_id IDS IN #{{ {}, {}, {}, }}",
+        uuid1, uuid2, uuid3
+    );
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload(payload)
+        .uri("/wql/query")
+        .to_request();
+
+    let mut resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+    let body = resp.take_body().as_str().to_string();
+    assert!(body.contains("\"a\": Integer(123)"));
+    assert!(body.contains("\"b\": Float(52.3)"));
+    assert!(body.contains("\"b\": Float(32.3)"));
+    assert!(body.contains(uuid1));
+    assert!(body.contains(uuid2));
+    assert!(body.contains(uuid3));
+}
+
+#[ignore]
+#[actix_rt::test]
+async fn test_select_keys_ids_post_ok() {
+    let mut app = test::init_service(App::new().configure(routes)).await;
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("CREATE ENTITY test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let _ = test::call_service(&mut app, req).await;
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 123, b: 12.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid1 = &body[(body.len() - 36)..];
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 456, b: 52.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid2 = &body[(body.len() - 36)..];
+
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload("INSERT {a: 543, b: 32.3,} INTO test_select_all_id")
+        .uri("/wql/tx")
+        .to_request();
+
+    let mut resp_insert = test::call_service(&mut app, req).await;
+    let body = resp_insert.take_body().as_str().to_string();
+    let uuid3 = &body[(body.len() - 36)..];
+
+    let payload = format!(
+        "Select #{{a,}} FROM test_select_all_id IDS IN #{{ {}, {}, {}, }}",
+        uuid1, uuid2, uuid3
+    );
+    let req = test::TestRequest::post()
+        .header("Content-Type", "application/wql")
+        .set_payload(payload)
+        .uri("/wql/query")
+        .to_request();
+
+    let mut resp = test::call_service(&mut app, req).await;
+
+    assert!(resp.status().is_success());
+    let body = resp.take_body().as_str().to_string();
+    assert!(body.contains("\"a\": Integer(123)"));
+    assert!(!body.contains("\"b\": Float(52.3)"));
+    assert!(!body.contains("\"b\": Float(32.3)"));
+    assert!(body.contains(uuid1));
+    assert!(body.contains(uuid2));
+    assert!(body.contains(uuid3));
+}
+
 trait BodyTest {
     fn as_str(&self) -> &str;
 }
