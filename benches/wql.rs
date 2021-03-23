598fbb12f50301e183aeff6107c31bde6acf7d99
naomijub <julia.boeira@creditas.com>
2021-03-23 16:42:46 -0300

    fmt

--- benches/wql.rs
+++ benches/wql.rs
@@ -4,23 +4,16 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use wql::Wql;
 
 fn criterion_benchmark(c: &mut Criterion) {
-
     c.bench_function("create_entity", |b| {
-        b.iter(|| {
-           Wql::from_str("create entity my_entity")
-        })
+        b.iter(|| Wql::from_str("create entity my_entity"))
     });
 
     c.bench_function("inser_entity", |b| {
-        b.iter(|| {
-           Wql::from_str("insert {a: 1} into my_entity")
-        })
+        b.iter(|| Wql::from_str("insert {a: 1} into my_entity"))
     });
 
     c.bench_function("select_all", |b| {
-        b.iter(|| {
-           Wql::from_str("select * from my_entity")
-        })
+        b.iter(|| Wql::from_str("select * from my_entity"))
     });
 }
 
